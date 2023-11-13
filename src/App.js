f84f64654a27169fa24752b54398d1a068210eab
Lucas Passos <lucaspassos2005@gmail.com>
2023-11-13 13:13:41 -0300

    Alterações na Página

--- src/App.js
+++ src/App.js
@@ -1,24 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Row, Col, Form, Table } from "react-bootstrap";
+
+const weekdays = ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"];
 
 function App() {
+  const [targetHours, setTargetHours] = useState(50);
+  const [weekHours, setWeekHours] = useState(Array(7).fill(0));
+
+  // Carrega o estado salvo no localStorage ao iniciar
+  useEffect(() => {
+    const savedWeekHours = JSON.parse(localStorage.getItem("weekHours"));
+    if (savedWeekHours) {
+      setWeekHours(savedWeekHours);
+    }
+  }, []);
+
+  // Salva o estado no localStorage sempre que houver mudanças
+  useEffect(() => {
+    localStorage.setItem("weekHours", JSON.stringify(weekHours));
+  }, [weekHours]);
+
+  const handleHourChange = (day, hours) => {
+    const newWeekHours = [...weekHours];
+    newWeekHours[day] = hours;
+    setWeekHours(newWeekHours);
+  };
+
+  const calculateTotalHours = () => {
+    return weekHours.reduce((total, hours) => total + hours, 0) * 4; // Multiplicando por 4 para considerar quatro semanas
+  };
+
+  const hoursLeft = targetHours - calculateTotalHours();
+  const extraHours = calculateTotalHours() - targetHours;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container style={{ backgroundColor: "#fff", padding: "20px", borderRadius: "10px", color: "#6a5acd" }}>
+      <h1 className="mt-5">Horas Trabalhadas</h1>
+      <Form className="mt-3">
+        <Form.Group controlId="targetHours">
+          <Form.Label>Escolha o alvo de horas para o mês:</Form.Label>
+          <Form.Control
+            as="select"
+            value={targetHours}
+            onChange={(e) => setTargetHours(parseInt(e.target.value))}
+          >
+            <option value={15}>15 horas</option>
+            <option value={30}>30 horas</option>
+            <option value={50}>50 horas</option>
+          </Form.Control>
+        </Form.Group>
+      </Form>
+      <Table bordered className="mt-3" style={{ backgroundColor: "#fff", borderRadius: "10px" }}>
+        <thead>
+          <tr>
+            <th style={{ backgroundColor: "#6a5acd", color: "#fff", borderTopLeftRadius: "10px" }}>Dia</th>
+            <th style={{ backgroundColor: "#6a5acd", color: "#fff", borderTopRightRadius: "10px" }}>Horas</th>
+          </tr>
+        </thead>
+        <tbody>
+          {weekHours.map((hours, index) => (
+            <tr key={index}>
+              <td>{weekdays[index]}</td>
+              <td>
+                <Form.Control
+                  type="number"
+                  min="0"
+                  value={hours}
+                  onChange={(e) => handleHourChange(index, parseInt(e.target.value))}
+                />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+      <Row className="mt-3">
+        <Col>
+          <p style={{ fontSize: "18px", fontWeight: "bold" }}>Total de horas por semana: {calculateTotalHours() / 4}</p>
+          <p style={{ fontSize: "18px", fontWeight: "bold" }}>Total de horas no mês: {calculateTotalHours()}</p>
+          {extraHours > 0 && <p style={{ fontSize: "18px", fontWeight: "bold" }}>Você atingiu o alvo e passou {extraHours} horas.</p>}
+          {hoursLeft > 0 && <p style={{ fontSize: "18px", fontWeight: "bold" }}>Faltam {hoursLeft} horas para atingir o alvo.</p>}
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
