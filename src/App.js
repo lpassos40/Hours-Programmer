d4d64fe7b0bc3d697e402374355317d859412f2d
Lucas Passos <lucaspassos2005@gmail.com>
2023-11-14 11:18:12 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container, Row, Col, Form, Table } from "react-bootstrap";
+import "./App.css"; // Importa o arquivo CSS para estilos personalizados
 
 const weekdays = ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"];
 
 function App() {
   const [targetHours, setTargetHours] = useState(50);
-  const [weekHours, setWeekHours] = useState(Array(7).fill(0));
+  const [weekHours, setWeekHours] = useState(Array(7).fill(null));
 
   // Carrega o estado salvo no localStorage ao iniciar
   useEffect(() => {
@@ -23,12 +24,13 @@ function App() {
 
   const handleHourChange = (day, hours) => {
     const newWeekHours = [...weekHours];
-    newWeekHours[day] = hours;
+    // Se o campo estiver vazio, considera como null para representar uma célula vazia
+    newWeekHours[day] = hours === "" ? null : parseFloat(hours);
     setWeekHours(newWeekHours);
   };
 
   const calculateTotalHours = () => {
-    return weekHours.reduce((total, hours) => total + hours, 0) * 4; // Multiplicando por 4 para considerar quatro semanas
+    return weekHours.reduce((total, hours) => total + (hours || 0), 0) * 4; // Multiplicando por 4 para considerar quatro semanas
   };
 
   const hoursLeft = targetHours - calculateTotalHours();
@@ -51,23 +53,24 @@ function App() {
           </Form.Control>
         </Form.Group>
       </Form>
-      <Table bordered responsive className="mt-3" style={{ backgroundColor: "#fff", borderRadius: "10px" }}>
-        <thead style={{  borderRadius: "10px" }}>
-          <tr>
-            <th style={{ backgroundColor: "#6a5acd", color: "#fff",  }}>Dia</th>
-            <th style={{ backgroundColor: "#6a5acd", color: "#fff",  }}>Horas</th>
+      <Table bordered responsive className="mt-3">
+        <thead >
+          <tr >
+            <th style={{backgroundColor: "#6a5acd", color: "#fff"}}>Dia</th>
+            <th style={{backgroundColor: "#6a5acd", color: "#fff"}}>Horas</th>
           </tr>
         </thead>
         <tbody>
-          {weekHours.map((hours, index) => (
+          {weekdays.map((day, index) => (
             <tr key={index}>
-              <td>{weekdays[index]}</td>
+              <td>{day}</td>
               <td>
                 <Form.Control
                   type="number"
+                  step="0.1"
                   min="0"
-                  value={hours}
-                  onChange={(e) => handleHourChange(index, parseInt(e.target.value))}
+                  value={weekHours[index] === null ? "" : weekHours[index]}
+                  onChange={(e) => handleHourChange(index, e.target.value)}
                 />
               </td>
             </tr>
@@ -78,7 +81,7 @@ function App() {
         <Col>
           <p style={{ fontSize: "18px", fontWeight: "bold", color: "#6a5acd" }}>Total de horas por semana: {calculateTotalHours() / 4}</p>
           <p style={{ fontSize: "18px", fontWeight: "bold", color: "#6a5acd" }}>Total de horas no mês: {calculateTotalHours()}</p>
-          {extraHours > 0 && <p style={{ fontSize: "18px", fontWeight: "bold", color: "#6a5acd" }}>Você atingirá o alvo com {extraHours} horas de sobra.</p>}
+          {extraHours > 0 && <p style={{ fontSize: "18px", fontWeight: "bold", color: "#6a5acd" }}>Você atingiu o alvo e passou {extraHours} horas.</p>}
           {hoursLeft > 0 && <p style={{ fontSize: "18px", fontWeight: "bold", color: "#6a5acd" }}>Faltam {hoursLeft} horas para atingir o alvo.</p>}
         </Col>
       </Row>
